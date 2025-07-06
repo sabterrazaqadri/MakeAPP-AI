d6186705d97b3596d21ca6fcad7dac27b891f930
sabter iqbal <sabterrazaqadri@gmail.com>
2025-07-06 19:45:50 +0500

    Updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api/generate", "/api/deploy", "/(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/api/webhook"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
